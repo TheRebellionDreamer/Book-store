e7b4c0531ed2a0085f981c25de12ed9979710e3a
Hizakashi <86956786+Hizakashi@users.noreply.github.com>
2021-10-05 13:38:12 +0300

    renamed action-creators of shopList

--- src/store/action-creators/addItem.action-creators.ts
+++ src/store/action-creators/addItem.action-creators.ts
@@ -1,6 +1,6 @@
 import { IGood } from "../../interfaces"
 import { ADD_ITEM_ACTION, shopListActions } from "../types/shopList.types"
 
-export const addItem = (payload: IGood): ADD_ITEM_ACTION => {
+export const createAddItem = (payload: IGood): ADD_ITEM_ACTION => {
   return { type: shopListActions.ADD_ITEM, payload: payload }
 }
--- src/store/actions/shopList.actions.ts
+++ src/store/actions/shopList.actions.ts
@@ -1,12 +1,12 @@
 import { Dispatch } from "react";
 import { IGood } from "../../interfaces";
-import { addItem } from "../action-creators/addItem.action-creators";
-import { removeItem } from "../action-creators/removeItem.action-creators";
+import { createAddItem } from "../action-creators/addItem.action-creators";
+import { createRemoveItem } from "../action-creators/removeItem.action-creators";
 import { ShopListAction } from "../types/shopList.types";
 
 export const addInBag = (good: IGood) => (dispatch: Dispatch<ShopListAction>) =>
-  dispatch(addItem(good));
+  dispatch(createAddItem(good));
 
 export const removeInBag =
   (id: IGood["id"]) => (dispatch: Dispatch<ShopListAction>) =>
-    dispatch(removeItem(id));
+    dispatch(createRemoveItem(id));
